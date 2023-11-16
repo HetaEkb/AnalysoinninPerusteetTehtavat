2252019f563f7951daa8faa539d4a9085f56b118
HetaEkb <heta23005@student.hamk.fi>
2023-11-16 12:21:38 +0200

    Update IfStatementExercises.py
    Ifstatements2

--- IfStatementExercises.py
+++ IfStatementExercises.py
@@ -27,3 +27,28 @@ if number1 != number2:
     print(str(number1) + " is not equal to " + str(number2))
 else:
      print(str(number1) + " is equal to " + str(number2))
+
+
+#2 
+number1 = 1
+number2 = 2
+number3 = 3 
+
+# a.
+if number1 == number2 == number3:
+    print("all numbers are equal")
+else:
+    print("not all numbers are equal")
+
+# b. 
+if (number1 == number2) or (number2 == number3):
+    print(str(number1)) + "and" + (str(number2)) + "are equal or" + (str(number2)) + "and" + (str(number3) )
+else:
+    print("neither is true") 
+
+# c.
+if (number1 > number2) and (number1 > number3):
+    print(str(number1)) + "is greater than" + (str(number2)) + "and" + (str(number1)) + "is greater than" + (str(number3) )
+else:
+    print("not true")
+
