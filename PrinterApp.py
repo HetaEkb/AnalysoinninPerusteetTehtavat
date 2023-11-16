838fe2f0d31fe2bd67250b71a3ead7b1da25fbda
HetaEkb <heta23005@student.hamk.fi>
2023-11-16 10:56:28 +0200

    PrinterApp
    PrinterApp tehtävä 2

--- PrinterApp.py
+++ PrinterApp.py
@@ -1,3 +1,24 @@
 # %%
+
+
 print("Printer App")
-print("This app is made by;")
+print("This app is made by:")
+creator = "Heta"
+print(creator)
+
+number1 = 2
+number2 = 3
+
+#print(number1)
+
+print("The value of number1 is", number1)
+
+print("The value of number2 is 3")
+
+multiplication = 0 
+
+print(number1 * number2)
+
+print(number1 - number2)
+print(number1 + number2)
+print(number1 / number2)
