222d57d0d3c6bbc54dbf3a41ad4aacabbdd96587
HetaEkb <heta23005@student.hamk.fi>
2023-11-16 11:58:12 +0200

    IfStatementExercises
    IfStatement

---
+++ IfStatementExercises.py
@@ -0,0 +1,29 @@
+# %%
+
+# 1. 
+number1 = 4
+number2 = 6 
+
+# a. 
+if number1 == number2:
+    print(str(number1) + " is equal to " + str(number2))
+else:
+    print(str(number1) + " is not equal to " + str(number2))
+
+# b.
+if number1 > number2:
+    print(str(number1) + " is greater than " + str(number2))
+else:
+    print(str(number1) + " is not greater than " + str(number2))
+
+# c. 
+if number1 >= number2: 
+   print(str(number1) + " is greater or equal to " + str(number2))
+else:
+    print(str(number1) + " is not greater or equal to " + str(number2))
+
+# d.
+if number1 != number2:
+    print(str(number1) + " is not equal to " + str(number2))
+else:
+     print(str(number1) + " is equal to " + str(number2))
